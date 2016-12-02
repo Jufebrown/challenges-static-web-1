c939a178bd1b399faab681ac46c21fed3f100d69
Jufebrown <jufebrown@gmail.com>
2016-12-02 14:20:22 -0600

    added js to keep range and number inputs the same

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 var num = document.querySelector(".number");
 var rng = document.querySelector(".range");
+
+console.log("num", num.value);
+console.log("rng", rng.value);
+
+function rngSetSame() {
+  event.preventDefault();
+  rng.value = num.value;
+}
+
+function numSetSame() {
+  event.preventDefault();
+  num.value = rng.value;
+}
+
+function stopReload(keyboardEvent) {
+
+  event.preventDefault();
+  if(keyboardEvent.key === "Enter"){
+    rngSetSame();
+  }
+}
+
+num.addEventListener("change", rngSetSame);
+rng.addEventListener("change", numSetSame);
+num.addEventListener("keyup", stopReload);
